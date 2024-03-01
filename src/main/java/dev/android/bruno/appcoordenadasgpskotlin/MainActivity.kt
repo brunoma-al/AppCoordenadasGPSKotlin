37d79e9d5a1428c6aeae69c82b91dd8c81091132
Bruno <brugacag@gmail.com>
2024-03-01 06:37:34 -0300

    Final commit.

--- src/main/java/dev/android/bruno/appcoordenadasgpskotlin/MainActivity.kt
+++ src/main/java/dev/android/bruno/appcoordenadasgpskotlin/MainActivity.kt
@@ -1,17 +1,43 @@
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import dev.android.bruno.appcoordenadasgpskotlin.R
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import dev.android.bruno.appcoordenadasgpskotlin.Manifest
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    var permissoesRequeridas = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    val APP_PERMISSOES_ID = 2024
+
+    var txtValorLatitude: TextView? = null
+    var txtValorLongitude: TextView? = null
+
+    var latitude: Double = 0.00
+    var longitude: Double = 0.00
+
+    var locationManager: LocationManager? = null
+
+    var gpsAtivo = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +46,126 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
+        txtValorLatitude = findViewById(R.id.txtValorLatitude)
+        txtValorLongitude = findViewById(R.id.txtValorLongitude)
+
+        locationManager =
+            application.getSystemService(LOCATION_SERVICE) as LocationManager
+
+
+        gpsAtivo = locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (gpsAtivo) {
+            obterCoordenadas()
+        } else {
+            latitude = 0.00
+            longitude = 0.00
+
+            txtValorLatitude!!.setText(formatarGeopoint(latitude))
+            txtValorLongitude!!.setText(formatarGeopoint(longitude))
+
+            Toast.makeText(this,
+                "Coordenadas não Disponíveis", Toast.LENGTH_LONG).show()
+
+
+        }
+
+
+    }
+
+    private fun obterCoordenadas() {
+
+        val permissaoAtiva: Boolean =
+            solicitarPermissaoParaObterLocalizacao()
+
+        if (permissaoAtiva)
+            capturarUltimaLocalizacaoValida()
+
+
+    }
+
+    private fun solicitarPermissaoParaObterLocalizacao(): Boolean {
+
+        Toast.makeText(this,
+            "Verificando permissões....",
+            Toast.LENGTH_LONG).show()
+
+        val permissoesNegadas: MutableList<String> =
+            ArrayList()
+
+        var permissaoNegada: Int
+
+        // permissoesRequeridas
+        for (permissao in permissoesRequeridas) {
+
+            permissaoNegada = ContextCompat.checkSelfPermission(
+                this@MainActivity, permissao
+            )
+
+            if (permissaoNegada != PackageManager.PERMISSION_GRANTED) {
+                permissoesNegadas.add(permissao)
+            }
+
+        }
+
+        return if (!permissoesNegadas.isEmpty()) {
+
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                permissoesNegadas.toTypedArray(),
+                APP_PERMISSOES_ID
+            )
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun capturarUltimaLocalizacaoValida() {
+
+        @SuppressLint("MissingPermission")
+        val location = locationManager!!.getLastKnownLocation(
+            LocationManager.GPS_PROVIDER
+        )
+
+        if (location != null) {
+
+            latitude = location.latitude
+            longitude = location.longitude
+
+        } else {
+
+            latitude = 0.00
+            longitude = 0.00
+
+        }
+
+        txtValorLatitude!!.setText(formatarGeopoint(latitude))
+        txtValorLongitude!!.setText(formatarGeopoint(longitude))
+
+        Toast.makeText(this,
+            "Coordenadas obtidas com sucesso", Toast.LENGTH_LONG).show()
+
+    }
+
+    private fun formatarGeopoint(valor: Double): String? {
+
+        val decimalFormat = DecimalFormat("#.####")
+
+        return decimalFormat.format(valor)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
 
-        var nMap = googleMap
+        mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
+        val sydney = LatLng(latitude, longitude)
         mMap.addMarker(MarkerOptions().position(sydney).title("Celular Localizado AQUI!"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
         mMap.uiSettings.setZoomControlsEnabled(true)
         mMap.setMinZoomPreference(9.3f)
         mMap.setMaxZoomPreference(12.0f)
-
     }
 }
